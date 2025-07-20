a136cb6f1b49aca2a01a9483bdef88fcf7e6c5dc
shark-Vu <khanhvu.daklak@gmail.com>
2025-07-20 20:38:32 +0700

    Update ad.js

--- modules/commands/ad.js
+++ modules/commands/ad.js
@@ -14,5 +14,5 @@ this.config = {
 this.run = async ({ api: { shareContact }, event: { threadID, messageReply, senderID, mentions, type }, args }) => {
   let id = Object.keys(mentions).length > 0 ? Object.keys(mentions)[0].replace(/\&mibextid=ZbWKwL/g, '') : args[0] !== undefined ? isNaN(args[0]) ? await global.utils.getUID(args[0]) : args[0] : senderID;
   if (type === "message_reply") id = messageReply.senderID;
-  shareContact("Liên hệ Admin !", 61568252515454, threadID);
+  shareContact("Liên hệ Admin !", 100050076122346, threadID);
 };
